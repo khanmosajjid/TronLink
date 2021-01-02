999b8bd2762c9522e7304a8628e070a18c5094c3
khanmosajjid <mosajjid.khan@gmail.com>
2021-01-03 00:22:41 +0530

    language changes

---
+++ src/i18n.js
@@ -0,0 +1,41 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    // we init with resources
+    resources: {
+      en: {
+        translations: {
+          "To get started, edit <1>src/App.js</1> and save to reload.":
+            "To get started, edit <1>src/App.js</1> and save to reload.",
+          "Welcome to React": "Welcome to React and react-i18next",
+          welcome: "Hello <br/> <strong>World</strong>"
+        }
+      },
+      de: {
+        translations: {
+          "To get started, edit <1>src/App.js</1> and save to reload.":
+            "Starte in dem du, <1>src/App.js</1> editierst und speicherst.",
+          "Welcome to React": "Willkommen bei React und react-i18next"
+        }
+      }
+    },
+    fallbackLng: "en",
+    debug: true,
+
+    
+    ns: ["translations"],
+    defaultNS: "translations",
+
+    keySeparator: false,
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
